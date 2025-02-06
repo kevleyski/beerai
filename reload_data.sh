8e215abfd0520fe02d4e70e581d8de472d5f4709
Kevin Staunton-Lambert <music@pyrmontbrewery.com.au>
2025-02-06 14:26:12 +1100

    KJSL Adding Voyager Craft Malt

--- reload_data.sh
+++ reload_data.sh
@@ -5,17 +5,19 @@ sleep 3
 
 neo4j stop
 neo4j-admin database import full --overwrite-destination --verbose \
-  --multiline-fields=true --nodes beer_styles.csv \
+  --multiline-fields=true --nodes=BeerStyle=beer_styles.csv \
   --nodes=BarleyVariety=barley_varieties.csv --nodes=MaltSpecification=malt_specifications.csv \
   --nodes=Farm=farms.csv --nodes=FreightOption=freight_options.csv \
   --nodes=Maltster=maltsters.csv --nodes=MaltCoA=malt_coas.csv \
   --nodes=Origin=origins.csv \
+  --nodes=Fermentable=fermentables.csv \
   --relationships=Produces=maltster_products_edges.csv \
   --relationships=ArrivesOn=freight_edges.csv \
-  --relationships=IsMalt=malt_edges.csv \
+  --relationships=MaltProduct=malt_edges.csv \
   --relationships=Pedigree=barley_pedigree_edges.csv \
   --relationships=GrownAt=malt_farm_edges.csv \
   --relationships=Origin=farm_origin_edges.csv \
-  --relationships=Origin=beer_style_origin_edges.csv
+  --relationships=Origin=beer_style_origin_edges.csv \
+  --relationships=TypicallyHas=beer_style_typically_contains_edges.csv
 
 neo4j start
